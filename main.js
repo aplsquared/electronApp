fa08299560d0046e57f10f807991fd5e19371cdc
aplsquared <apatidar@lsquared.com>
2019-04-03 10:28:25 +0530

    Auto update

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
+
+const {autoUpdater} = require('electron-updater')
+require('electron-debug')({showDevTools: true, enabled: true});
+// autoUpdater.updateConfigPath = path.join(_path to app-update.yml_);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -33,7 +37,27 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+
+// app.on('ready', createWindow)
+
+app.on('ready', function() {
+    createWindow()
+    autoUpdater.checkForUpdates();
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    win.webContents.send('updateReady')
+    alert("Hi");
+});
+
+setTimeout(() => {
+    // autoUpdater
+}, 1000);
+
+// when receiving a quitAndInstall signal, quit and install the new version ;)
+ipcMain.on("quitAndInstall", (event, arg) => {
+    autoUpdater.quitAndInstall();
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -50,3 +74,4 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
